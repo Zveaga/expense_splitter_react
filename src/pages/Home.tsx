4ad90a08792a2ddbd9c7410b98a5a31c53f62916
Rares <rares.rares.cox@gmail.com>
2025-02-08 16:32:18 +0100

    centered home page components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,27 +6,29 @@ import { Container, Box, Stack, InputBase, Divider, Typography, useTheme,  } fro
 const sampleEvents: Event[] = [
 	{
 		id: 1,
-		description: 'restaurant',
+		description: 'ski trip',
 		users: [user1, user2],
 		date: '21-01-2024',
 
 	},
 	{
 		id: 2,
-		description: 'boat',
+		description: 'greece',
 		users: [user1, user2],
 		date: '21-01-2024',
 
 	},
 	{
 		id: 3,
-		description: 'jeep ride',
+		description: 'patagonia',
 		users: [user1, user2],
 		date: '21-01-2024',
 	},
 
 
-]; 
+];
+
+
 
 const Home: React.FC = () => {
 	const theme = useTheme();
@@ -44,17 +46,20 @@ const Home: React.FC = () => {
 	const EventLine: React.FC<{event: Event}> = ({event})  => {
 		return (
 			<Stack
+				// width={'90%'}
 				onClick={(e: React.MouseEvent) => handleEventClick(e, event)}
 				direction={'row'}
 				bgcolor={theme.palette.primary.main}
 				paddingX={'0.5em'}
+				justifyContent= {'center'}
+				alignItems={'center'} 
 				sx={{
 					borderRadius: '5px',
 					cursor: 'pointer',
 					transition: 'padding-left ease-in-out 0.3s, padding-right ease-in-out 0.3s, border-radius ease-in-out 0.3s, background-color ease-in-out 0.3s',
 					'&:hover': {
 					  bgcolor: theme.palette.primary.dark,
-					//   borderRadius: '2em',
+					  borderRadius: '2em',
 					  paddingLeft: '1em',
 					  paddingRight: '0.02em',
 					},
@@ -73,14 +78,18 @@ const Home: React.FC = () => {
 			<Box
 				sx={{
 					padding: '1em',
-					width: '300px',
+					width: '50%',
 					height: '600px',
 					overflow: 'auto',
 					border: '2px solid #757575',
 					borderRadius: '1em',
+					mx: 'auto',
 				}}
 			>
-				<Stack gap={1}>
+			<Typography sx={{ textAlign: 'center'}} variant="h4" gutterBottom >
+				Events
+			</Typography>
+				<Stack sx={{ }} gap={1}>
 					{events.map((event, i) => (
 						<EventLine key={i} event={event} />
 					))}
